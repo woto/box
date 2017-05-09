f410811835156cd1f21aa93329a5235faa3c69e0
Ruslan Kornev <oganer@gmail.com>
2017-05-09 15:54:21 +0300

    tests

---
+++ test/system/communicates_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CommunicatesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit communicates_url
+  #
+  #   assert_selector "h1", text: "Communicate"
+  # end
+end
