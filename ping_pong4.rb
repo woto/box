47a2baafe3db891a2ba8ae0c4977b2a5a61ce91a
Ruslan Kornev <oganer@gmail.com>
2017-05-21 17:20:11 +0300

    ping_pong4

--- ping_pong4.rb
+++ ping_pong4.rb
@@ -1,7 +1,11 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'celluloid/debug'
 require 'celluloid/current'
 require 'celluloid/io'
 require 'json'
+
 require File.expand_path('config/environment', File.dirname(__FILE__))
 
 class CableServer
@@ -11,23 +15,15 @@ class CableServer
   finalizer :finalize
 
   def initialize(host, port)
-    subscribe "push_to_websocket", :push_to_websocket
     info "*** Starting CableServer on #{host}:#{port}"
+    # Since we included Celluloid::IO, we're actually making a
+    # Celluloid::IO::TCPServer here
     @server = TCPServer.new(host, port)
     async.run
   end
 
   def finalize
     @server.close if @server
-  end
-
-  def push_to_websocket(topic, data)
-    info "#{topic}: #{data}"
-    begin
-      ActionCable.server.broadcast("a", data)
-    rescue Encoding::UndefinedConversionError => e
-      info e
-    end
   end
 
   def run
@@ -39,7 +35,8 @@ class CableServer
     info "*** Received connection on CableServer from #{host}:#{port}"
     loop do
       read = socket.readpartial(4096)
-      publish "push_to_device", read
+      json = JSON.parse(read)
+      publish "push_to_device", json
     end
   rescue EOFError
     info "*** #{host}:#{port} disconnected from CableServer"
@@ -57,6 +54,8 @@ class DeviceServer
     @sockets = []
     subscribe "push_to_device", :push_to_device
     info "*** Starting DeviceServer on #{host}:#{port}"
+    # Since we included Celluloid::IO, we're actually making a
+    # Celluloid::IO::TCPServer here
     @server = TCPServer.new(host, port)
     async.run
   end
@@ -65,10 +64,18 @@ class DeviceServer
     @server.close if @server
   end
 
-  def push_to_device(topic, data)
-    info "#{topic}: #{data}"
-    @sockets.each do |socket|
-      async.write_to_socket(socket, data)
+  def push_to_device(topic, hash)
+    string = hash.to_json
+    info "#{topic} #{string}"
+    @sockets.each do |socket:, id:|
+      if hash['id'] == id
+        begin
+          socket.write string.encode('CP1251')
+        rescue IOError => e
+          info e.inspect
+          @sockets.delete @sockets.find { |s| s[:socket] == socket }
+        end
+      end
     end
   end
 
@@ -79,33 +86,39 @@ class DeviceServer
   def handle_connection(socket)
     _, port, host = socket.peeraddr
     info "*** Received connection on DeviceServer from #{host}:#{port}"
-    @sockets << socket
+    @sockets << {socket: socket, id: nil}
+
     loop do
       read = socket.readpartial(4096)
-      publish "push_to_websocket", read
+      socket.write "OK: #{read}"
+
+      info read
+      info read.bytes
+
+      begin
+        json = JSON.parse(read.force_encoding('CP1251').encode('UTF-8'))
+        socket_hash = @sockets.find { |s| s[:socket] == socket }
+        socket_hash[:id] = json["id"]
+      rescue TypeError, NoMethodError, ArgumentError, JSON::ParserError => e
+        raise ApplicationError.new(e.inspect)
+      end
+
+      InMessageJob.perform_later json
+
     end
-  rescue EOFError, Errno::ETIMEDOUT, Errno::ECONNRESET => e
+  rescue EOFError, Errno::ETIMEDOUT, Errno::ECONNRESET, ApplicationError => e
+    info e.inspect
     info "*** #{host}:#{port} disconnected from DeviceServer"
     socket.close
   end
-
-  private
-
-  def write_to_socket(socket, data)
-    begin
-      socket.write "#{data}"
-    rescue IOError => e
-      info "Rescued #{e}"
-      @sockets.delete socket
-    end
-  end
-
 end
 
-device_supervisor = DeviceServer.new("0.0.0.0", 8084)
-action_cable_supervisor = CableServer.new("0.0.0.0", 8085)
-trap("INT") { device_supervisor.terminate; exit }
-trap("INT") { action_cable_supervisor.terminate; exit }
+class ApplicationError < StandardError; end
+
+device_server = DeviceServer.new("0.0.0.0", 8084)
+cable_server = CableServer.new("0.0.0.0", 8085)
+trap("INT") { device_server.terminate; exit }
+trap("INT") { cable_server.terminate; exit }
 sleep
 
 #NOTE
