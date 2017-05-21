9cc5055df03225c54601469014b6d40f0dcaeaca
Ruslan Kornev <oganer@gmail.com>
2017-05-21 17:25:44 +0300

    ping_pong3

--- ping_pong3.rb
+++ ping_pong3.rb
@@ -2,6 +2,10 @@ require 'celluloid/current'
 require 'celluloid/io'
 require 'json'
 
+SUCCESS = 'success'
+FAILURE = 'failure'
+PENDING = 'pending'
+
 class Counter
   # This is all you have to do to turn any Ruby class into one which creates
   # Celluloid actors instead of normal objects
@@ -40,6 +44,25 @@ class EchoServer
     loop { async.handle_connection @server.accept }
   end
 
+  def send_message(result, socket, counter, bytes, message, encoding)
+    raise 'result should be a fixed string' unless [SUCCESS, PENDING, FAILURE].include?(result)
+    raise 'counter should be an integer' unless counter.is_a?(Integer)
+    raise 'message should be a string' unless message.is_a?(String)
+    response = {result: result, message: message}
+    response[:time] = Time.now
+    response[:bytes] = bytes
+    response[:counter] = counter
+    json = begin
+      response.to_json
+    rescue Encoding::UndefinedConversionError, JSON::GeneratorError => e
+      error = "Generate error: #{SecureRandom.uuid}"
+      puts error
+      response[:message] = error
+      response.to_json
+    end
+    socket.write json.encode(encoding)
+  end
+
   def handle_connection(socket)
     _, port, host = socket.peeraddr
     puts "*** Received connection from #{host}:#{port}"
@@ -47,20 +70,33 @@ class EchoServer
 
     loop do
       read = socket.readpartial(4096)
-      puts read.bytes
-      response = {}
-      response[:time] = Time.now
-      response[:bytes] = read.bytes
-      response[:string] = read.force_encoding(Encoding::CP1251)
-      response[:counter] = actor.increment
+      counter = actor.increment
+      puts read
+      print read.bytes
+      puts
+
       begin
-        json = response.to_json
-      rescue StandardError => e
-        json = e.message
+        json = JSON.parse(read)
+      rescue JSON::ParserError => e
+        error = "Parse error: #{SecureRandom.uuid}"
+        puts error
+        send_message(FAILURE, socket, counter, read.bytes, error, 'Windows-1252')
+        raise 'exit'
       end
-      socket.write json
+
+      begin
+        message = json['message'].force_encoding(json['encoding'])
+      rescue TypeError, NoMethodError, ArgumentError => e
+        error = "Encoding error: #{SecureRandom.uuid}"
+        puts error
+        send_message(FAILURE, socket, counter, read.bytes, error, 'Windows-1252')
+        raise 'exit'
+      end
+
+      send_message(SUCCESS, socket, counter, read.bytes, message, json['encoding'])
     end
-  rescue EOFError, Errno::ECONNRESET
+  rescue StandardError, EOFError, Errno::ECONNRESET => e
+    puts e.message
     puts "*** #{host}:#{port} disconnected"
     actor.terminate
     socket.close
